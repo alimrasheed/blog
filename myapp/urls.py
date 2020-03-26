b10d0c9ccf508c36c504af36243dda4243cb219d
alimrasheed <m97alim@gmail.com>
2020-03-26 23:17:18 +0300

    Views and urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from pages.views import home_view, contact_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('home/',home_view,name='home'),
     path('admin/', admin.site.urls),
+    path('contact/', contact_view, name='Contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(*args, **kwargs):
+    print(args)
     return HttpResponse("<h1>Hello World</h1>")
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")    
