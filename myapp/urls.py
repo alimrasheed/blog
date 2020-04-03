9225de1f964564226ceac0eab5c02fcacddaa52a
Alim <alim@localhost.localdomain>
2020-04-03 21:26:30 +0300

    Playing around with templates, views and urlconf

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view, contact_view
+from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('home/',home_view,name='home'),
     path('admin/', admin.site.urls),
-    path('contact/', contact_view, name='Contact')
+    path('contact/', contact_view, name='Contact'),
+    path('about/', about_view, name='About')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    print(args)
-    return HttpResponse("<h1>Hello World</h1>")
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, 'home.html', {})
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")    
+def contact_view(request, *args, **kwargs):
+    return render(request, 'contact.html', {})
+
+def about_view(request, *args, **kwargs):
+    return render(request, 'about.html', {}) 
